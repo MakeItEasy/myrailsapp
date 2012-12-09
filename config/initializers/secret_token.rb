aeae73046f198aa547cbb8536f8720968208a662
root <root@azami-office-management-dev.(none)>
2012-12-09 17:09:47 +0900

    init my rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrailsapp::Application.config.secret_token = 'd88c4eb567e9d7b70cb3e4811b27ddcdc68e89b5d1f762cb6f81ff6ea5297601feb56d58c9a0743af4e5d65206f7cfdd97cd5043941635e15e2a74b7c7754df4'
