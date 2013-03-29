b3167e0596752e970e263125dfbaf1fa59e27bac
MakeItEasy <rugang6891@gmail.com>
2013-03-29 13:48:57 +0900

    流程测试可以通过了。 注意点： 1. 在post，get请求中具体的参数应该是什么

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -1,19 +1,31 @@
+# encoding: UTF-8
+
 require 'test_helper'
 
 class UserFlowsTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:dairg)
+    @user_confirmed = users(:user_confirmed)
+    @user_unconfirmed = users(:user_unconfirmed)
   end
-  # test "the truth" do
-  #   assert true
-  # end
 
   test "login and browse site" do
+    get_via_redirect '/user/dashboard/index'
+    assert_equal '/auth/login', path
+
     get '/auth/login'
     assert_response :success
 
-    # post_via_redirect "/auth/login", :email => 'rugang6891@gmail.com', :password => '222222'
-    post_via_redirect "/auth/login", :email => @user.email, :password => @user.password
+    post_via_redirect user_session_path, :user => {:email => @user_confirmed.email, :password => '123456'}
     assert_equal '/user/dashboard/index', path
+    assert_equal flash[:notice], I18n.t('devise.sessions.signed_in')
+
+    get '/user/dashboard/index'
+    assert_response :success
+  end
+
+  test "should comfirm your account" do
+    post_via_redirect user_session_path, :user => {:email => @user_unconfirmed.email, :password => '123456'}
+    assert_equal '/auth/login', path
+    assert_equal flash[:alert], I18n.t('devise.failure.unconfirmed')
   end
 end
